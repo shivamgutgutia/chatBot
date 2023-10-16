43459175f0a3959a4661c91cabbd6d194ff81c2d
Shivam Gutgutia <gutgutia159@gmail.com>
2023-10-16 23:48:54 +0530

    Added functionalities like users, chatrooms, usernames etc

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
 const express = require("express")
 const http = require("http")
 const socketio = require("socket.io")
+const formatMessage = require("./utils/message")
+const {userJoin,getCurrentUser} = require("./utils/users") 
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+
 app.use(express.static("./public"))
 
+const botName = "ChatApp"
+
 io.on("connection",(socket)=>{
+    
+    socket.on("joinRoom",({username,room})=>{
+
+        const user = userJoin(socket.id,username,room) 
+        socket.join(user.room)
+
+
+        //User join message
+        socket.emit("message",formatMessage(botName,"Welcome to chatroom"))
+
+        //When a user connects
+        socket.broadcast.to(user.room).emit("message",formatMessage(botName,`${user.username} has joined the chatroom`)) 
+    })
+
     console.log("New connection")
 
-    socket.emit("message","Welcome to chatroom")
+    
+
+    //When a user disconnects
+    socket.on("disconnect",()=>{
+        //const user = getCurrentUser(socket.id)
+        io.emit("message",formatMessage(botName, `A user has left the chat`))
+    })
+
+    //Listen for chat message
+
+    socket.on("chatMessage",msg=>{
+        const user = getCurrentUser(socket.id)
+        io.to(user.room).emit("message",formatMessage(user.username,msg))
+    })
+
 })
 
 const port = process.env.PORT||3000
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,44 @@
+chatForm = document.getElementById("chat-form")
+const chatMessages = document.querySelector(".chat-messages")
 const socket = io()
 
+//Get username and room from the URL
+const {username,room}=Qs.parse(location.search,{ignoreQueryPrefix:true})
+
+//Join chatroom
+socket.emit("joinRoom",{username,room})
+
+//Message from server
 socket.on("message", message=>{
-    console.log(message)
+    outputMessage(message)
+
+    //Scroll
+    chatMessages.scrollTop = chatMessages.scrollHeight
 })
+
+
+//Message submit
+chatForm.addEventListener("submit",(event)=>{
+    event.preventDefault()
+
+    //Get message text
+    const msg=event.target.elements.msg.value
+
+    //Emit message to server
+    socket.emit("chatMessage",msg)
+
+    //Clear input
+    event.target.elements.msg.value=""
+    event.target.elements.msg.focus()
+
+})
+
+function outputMessage(message){
+    const div = document.createElement("div")
+    div.classList.add("message")
+    div.innerHTML = `<p class="meta">${message.user} <span>${message.time}</span></p>
+    <p class="text">
+        ${message.text}
+    </p>`
+    document.querySelector(".chat-messages").appendChild(div)
+}
